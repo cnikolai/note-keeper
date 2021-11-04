31bd499428aaa23d74d75a592840e201e61a98f8
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-11-04 19:03:16 -0400

    Implemented subject and note text inputs fields.

--- app/src/main/java/edu/cnm/deepdive/notekeeper/controller/NoteFragment.java
+++ app/src/main/java/edu/cnm/deepdive/notekeeper/controller/NoteFragment.java
@@ -3,9 +3,11 @@ package edu.cnm.deepdive.notekeeper.controller;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import edu.cnm.deepdive.notekeeper.R;
 import edu.cnm.deepdive.notekeeper.databinding.FragmentNoteBinding;
@@ -19,6 +21,8 @@ public class NoteFragment extends Fragment {
       LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     binding = FragmentNoteBinding.inflate(inflater, container, false);
+    binding.addNote.setOnClickListener(
+        this::onClick);
     return binding.getRoot();
   }
 
@@ -30,5 +34,10 @@ public class NoteFragment extends Fragment {
   public void onDestroyView() {
     super.onDestroyView();
     binding = null;
+  }
+
+  private void onClick(View v) {
+    Navigation.findNavController(binding.getRoot())
+        .navigate(NoteFragmentDirections.openNote()); //generated for us from fragment
   }
 }
